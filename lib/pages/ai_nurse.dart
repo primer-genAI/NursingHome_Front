75ecefd80b8ff514357b9a814c160029a3ec28e2
leejunghoon <coronarita1991@gmail.com>
2024-09-03 00:01:09 +0900

    Modify : Improve UX inserting '>' to button

--- lib/pages/ai_nurse.dart
+++ lib/pages/ai_nurse.dart
@@ -58,12 +58,23 @@ class AINursePage extends StatelessWidget {
                       MaterialPageRoute(builder: (context) => ChatPage(nurseIdx: 2)),
                     );
                   },
-                  child: _buildAIConsultantTile(
-                    'AI 상담사 간단이',
-                    'assets/images/nurse2.png',
-                    '전문적이고 요점만 간단히 환자분의 상황을 알려드립니다.',
-                    iconSize,
-                    fontSizeSubtitle,
+                  child: Container(
+                    padding: EdgeInsets.all(16.0), // 내부 패딩 추가
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent.withOpacity(0.1), // 배경색 설정
+                      borderRadius: BorderRadius.circular(16.0), // 모서리 둥글게 설정
+                      border: Border.all(
+                        color: Colors.blueAccent, // 테두리 색상 설정
+                        width: 0.0, // 테두리 두께 설정
+                      ),
+                    ),
+                    child: _buildAIConsultantTile(
+                      'AI 상담사 간단이',
+                      'assets/images/nurse2.png',
+                      '전문적이고 요점만 간단히 환자분의 상황을 알려드립니다.',
+                      iconSize,
+                      fontSizeSubtitle,
+                    ),
                   ),
                 ),
                 SizedBox(height: screenHeight * 0.03),
@@ -74,12 +85,23 @@ class AINursePage extends StatelessWidget {
                       MaterialPageRoute(builder: (context) => ChatPage(nurseIdx: 1)),
                     );
                   },
-                  child: _buildAIConsultantTile(
-                    'AI 상담사 친절이',
-                    'assets/images/nurse1.png',
-                    '친절하고 자세하게 환자분의 상황을 설명해드리겠습니다.',
-                    iconSize,
-                    fontSizeSubtitle,
+                  child: Container(
+                    padding: EdgeInsets.all(16.0), // 내부 패딩 추가
+                    decoration: BoxDecoration(
+                      color: Colors.redAccent.withOpacity(0.1), // 배경색 설정
+                      borderRadius: BorderRadius.circular(16.0), // 모서리 둥글게 설정
+                      border: Border.all(
+                        color: Colors.greenAccent, // 테두리 색상 설정
+                        width: 0.0, // 테두리 두께 설정
+                      ),
+                    ),
+                    child: _buildAIConsultantTile(
+                      'AI 상담사 친절이',
+                      'assets/images/nurse1.png',
+                      '친절하고 자세하게 환자분의 상황을 설명해드리겠습니다.',
+                      iconSize,
+                      fontSizeSubtitle,
+                    ),
                   ),
                 ),
               ],
